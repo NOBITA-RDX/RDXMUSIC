b40b4445cc9e77093e68c6a26131ff77b6d20366
RDX RAJ <141653116+NOBITA-RDX@users.noreply.github.com>
2024-02-08 21:35:36 +0530

    Update purge.py

--- RDXMUSIC/plugins/admins/purge.py
+++ RDXMUSIC/plugins/admins/purge.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import MessageDeleteForbidden, RPCError
 from pyrogram.types import Message
-from RDX.utils.daxx_ban import admin_filter
+from RDX.utils.rdx_ban import admin_filter
 from RDX import app
 
 
